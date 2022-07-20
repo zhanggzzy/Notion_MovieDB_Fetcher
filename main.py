c70f7c5f71e539bf4846173c9c92b8473dbf65dd
Otto <zzy801997@163.com>
2022-07-21 02:39:12 +1000

    init notion connection

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from textwrap import indent
 import requests
 import json
 from private import TMDB_apikey, TMDB_api_token, Notion_token, Notion_DB_ID
@@ -43,9 +44,10 @@ def search_with_TMDB(query, api_key=TMDB_apikey, language="zh-CN", page=1,
         url += "&primary_release_year=" + primary_release_year
     
     movie_id = requests.get(url, headers=header).json()["results"][0]["id"]
+    # print(movie_id)
 
     if movie_id:
-        return movie_id
+        return int(movie_id)
     else:
         raise Exception("No movie found")
 
@@ -61,7 +63,7 @@ def get_TMDB_movie_detail(TMDB_id, api_key=TMDB_apikey, language="zh-CN"):
     url += str(TMDB_id)
     # url += "?api_key=" + TMDB_apikey
     url += "&language=" + language
-    movie_data = requests.get(url, header=header).json()
+    movie_data = requests.get(url, headers=header).json()
     return movie_data
 
 
@@ -76,8 +78,8 @@ def get_TMDB_movie_credits(TMDB_id, api_key=TMDB_apikey, language="zh-CN"):
     url += str(TMDB_id)
     url += "/credits"
     # url += "?api_key=" + TMDB_apikey
-    url += "&language=" + language
-    movie_credits = requests.get(url, header=header).json()
+    # url += "&language=" + language
+    movie_credits = requests.get(url, headers=header).json()
     return movie_credits
 
 
@@ -85,19 +87,93 @@ def get_TMDB_movie_poster(img_url):
     url = "https://image.tmdb.org/t/p/w1280" + img_url
     return url
 
-
+# FIXME: {'errors': ['id is not a valid integer']}
 def organize_TMDB_data(TMDB_id):
     movie_data = get_TMDB_movie_detail(TMDB_id)
     movie_credits = get_TMDB_movie_credits(TMDB_id)
-
-    
+    print(movie_data)
+    print(movie_credits)
     
     return movie_data, movie_credits
 
 
+def get_notion_info(Notion_token, Notion_DB_ID):
+    header = {
+        'Authorization': 'Bearer ' + Notion_token,
+        'Notion-Version': '2022-06-28'
+    }
+    
+    url = "https://api.notion.com/v1/databases/" + Notion_DB_ID
+    notion_info = requests.get(url, headers=header).json()
+    return notion_info
+
+
+def insert_into_notion(Notion_token, Notion_DB_ID, data):
+    header = {
+        'Authorization': 'Bearer ' + Notion_token,
+        'Notion-Version': '2022-06-28'
+    }
+    url = "https://api.notion.com/v1/pages"
+    response = requests.post(url, headers=header, json=data)
+    return response
+
 if __name__ == '__main__':
     # print('Movie Fetcher')
     result = search_with_TMDB(query="蝙蝠侠")
+    organize_TMDB_data(result)
     print(result)
-    # print(TMDB_apikey)
-    # print(TMDB_api_token)
+    data = {
+        "parent": {
+            "type": "database_id",
+            "database_id": Notion_DB_ID
+        },
+        "properties": {
+            "片名": {
+                "title": [
+                    {
+                        "type": "text",
+                        "text": {
+                            "content": "猫鼠游戏"
+                        }
+                    }
+                ]
+            },
+            "外语片名": {
+                "rich_text": [
+                    {
+                        "type": "text",
+                        "text": {
+                            "content": "Catch Me If You Can",  
+                        }
+                    }
+                ]
+            },
+            "分类": {
+                "select": {
+                    "name": "电影",
+                }
+            },
+            "导演": {
+                "Multi-Select": [
+                    {
+                        "name": "Steven Spielberg"
+                    }
+                ]
+            },
+            "主演": {
+                "Multi-Select": [
+                    {
+                        "name": "Leonardo DiCaprio"
+                    },
+                    {
+                        "name": "Tom Hanks"
+                    },
+                    # {
+                    #     ""
+                    # }
+                ]
+            }
+        }
+    }
+    # print(get_notion_info(Notion_token, Notion_DB_ID))
+    # print(insert_into_notion(Notion_token, Notion_DB_ID, data))
